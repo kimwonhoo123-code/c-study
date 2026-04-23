7614c80cb8fb593f138f592e6f3404fb614b1615
wonhoo <kimwonhoo123@gmail.com>
2026-04-23 16:54:46 +0900

    first

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+	string h;
+	getline(cin, h);
+
+	sort(h.begin(), h.end());
+
+	cout << h;
+
+}
